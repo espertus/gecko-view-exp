c45280fdd7aabaf5618f8fa8a8947c48d6d85310
Ellen Spertus <ellen.spertus@gmail.com>
2020-01-22 09:59:40 -0800

    Exhibit bug 1598696

--- app/src/main/java/mozilla/voice/geckoviewexperiment/MainActivity.kt
+++ app/src/main/java/mozilla/voice/geckoviewexperiment/MainActivity.kt
@@ -1,12 +1,22 @@
 package mozilla.voice.geckoviewexperiment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.mozilla.geckoview.GeckoRuntime
+import org.mozilla.geckoview.GeckoSession
 
 class MainActivity : AppCompatActivity() {
+    private val HOME_PAGE = "http://kitfox.tola.me.uk"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val session = GeckoSession()
+        val runtime = GeckoRuntime.create(this)
+
+        session.open(runtime)
+        geckoView.setSession(session)
+        session.loadUri(HOME_PAGE)
     }
 }
